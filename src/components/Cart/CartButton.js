3cac06f472cd21172604ae3e537f7e907774b08b
thecthebest <asyed5839@gmail.com>
2021-07-15 12:34:46 +0200

    Functionality to show quantity on the cart button
    To show the quantity UseSelector hook has been added to the code. The useSelector hook take state returned by redux and only extracts the information needed, in this case the item quantity in the cart.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 import { uiActions } from '../store/ui-slice';
 
@@ -7,10 +7,13 @@ const CartButton = (props) => {
   const toggleClickHandler = () => {
     dispatch(uiActions.toggle());
   };
+  const cartQuantity = useSelector((state) => {
+    return state.cart.totalQuantity;
+  });
   return (
     <button onClick={toggleClickHandler} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
