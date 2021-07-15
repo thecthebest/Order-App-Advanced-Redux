98c509d4b3f5f8ab205da28cb38fbdb13b35b09a
thecthebest <asyed5839@gmail.com>
2021-07-15 19:18:53 +0200

    Added a key and passed down an id
    A key props has been added that allow react to keep track of dynamically generated item. In Addition "id" has been added to the item props so that it can be passed down to the child component.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -15,8 +15,10 @@ const Cart = (props) => {
           cartItems.map((item) => {
             return (
               <CartItem
+              key={item.id}
                 item={
                   {
+                    id: item.id,
                     title: item.name,
                     quantity: item.quantity,
                     total: item.totalPrice,
