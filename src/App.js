58e3197aa6b24fb7218fb6be9631d9ce49a10ab2
thecthebest <asyed5839@gmail.com>
2021-07-16 18:47:13 +0200

    Added useEffect and useDispatch hooks
    The useEfffect hook is used to send the cart data stored in "cartdb" to Google firebase whereas useDispatch dispatches various actions according to the status of the http request. These various actions will be used by the Notification component to show different notifications.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import { uiActions } from './components/store/ui-slice';
 
 function App() {
+  const dispatch = useDispatch();
   const cart = useSelector((state) => {
     return state.ui.cartIsVisible;
   });
 
+  const cartdb = useSelector((state) => {
+    return state.cart;
+  });
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification(
+        {
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!'
+        }
+      ));
+      const response = await fetch('https://udemy-9885a-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cartdb),
+        }
+      );
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+      dispatch(uiActions.showNotification(
+        {
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!'
+        }
+      ));
+    };
+    sendCartData().catch((error) => {
+      dispatch(uiActions.showNotification(
+        {
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!'
+        }
+      ));
+    });
+  }, [cartdb, dispatch]);
   return (
     <Layout>
       {cart && <Cart />}
